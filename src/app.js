5cc502a9a2f84c3dd867200dcd3161a7c5e9b1be
Kleyton João <kleyton.joao@gmail.com>
2020-12-03 21:47:51 -0300

    inicial commit projeto criado

---
+++ src/app.js
@@ -0,0 +1,16 @@
+  const express = require('express');
+  const routes = require('./routes');
+
+  class App {
+    constructor(){
+      this.server = express();
+    }
+
+    middlewares(){
+      this.server.use(express.json());
+    }
+
+    routes() {
+      this.server.use()
+    }
+  }
