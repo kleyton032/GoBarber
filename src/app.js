86c0a4e787f34218740f9c53640ba406e3f2c52d
Kleyton João <kleyton.joao@gmail.com>
2020-12-04 17:09:36 -0300

    config sucrase e nodemon

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
-  const express = require('express');
-  const routes = require('./routes');
+  import express from 'express';
+  import routes from './routes';
 
   class App {
     constructor(){
       this.server = express();
+      this.middlewares();
+      this.routes();
     }
 
     middlewares(){
@@ -11,8 +13,8 @@
     }
 
     routes() {
-      this.server.use()
+      this.server.use(routes);
     }
   }
 
-  module.exports = new App().server;
+  export default new App().server;
