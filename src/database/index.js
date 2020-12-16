1336673eaea4d994620047eafea337a29759d198
Kleyton João <kleyton.joao@gmail.com>
2020-12-16 17:09:57 -0300

    criando model appointments

--- src/database/index.js
+++ src/database/index.js
@@ -1,24 +1,25 @@
- import Sequelize from 'sequelize';
- import databaseConfig from '../config/database';
- import User from '../app/models/User';
- import File from '../app/models/File';
+import Sequelize from 'sequelize';
+import databaseConfig from '../config/database';
+import User from '../app/models/User';
+import File from '../app/models/File';
+import Appointments from '../app/models/Appointments';
 
-const models = [User, File];
+const models = [User, File, Appointments];
 
- class Database {
+class Database {
 
-  constructor(){
+  constructor() {
     this.init();
   }
 
   init() {
     this.connection = new Sequelize(databaseConfig);
-    
+
     models
-    .map(model => model.init(this.connection))
-    .map(model => model.associate && model.associate(this.connection.models));
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 
- }
+}
 
- export default new Database();
+export default new Database();
