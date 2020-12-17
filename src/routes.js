9102d5ada5f8b3704ed71489380e1638ab81582e
Kleyton João <kleyton.joao@gmail.com>
2020-12-17 09:13:39 -0300

    criacao model migrations e controller appointment

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
-
+import AppointmentController from './app/controllers/AppointmentController';
 
 
 const routes = new Router();
@@ -20,5 +20,6 @@ routes.use(authMiddlewares);
 routes.put('/users', UserController.update);
 routes.post('/file', upload.single('file'), FileController.store);
 routes.get('/providers', ProviderController.index);
+routes.get('/appointments', AppointmentController.store);
 
 export default routes;
